722ab935f6271b2d5260e23f7eb3fce1c3809efb
Daniel Howe <dhowe@swipeclock.com>
2018-04-04 10:59:11 -0600

    add more tests around resource changes

--- test/test_resource.py
+++ test/test_resource.py
@@ -48,3 +48,89 @@ class TestResources(TestQless):
 
         self.assertEqual(res['locks'], ['jid-1'])
         self.assertEqual(res['pending'], {})
+
+    def test_handles_increasing_resource_limit(self):
+        """Resource will eventually raise when jobs complete"""
+        self.lua('resource.set', 0, 'r-1', 2)
+
+        self.lua('put', 0, None, 'queue', 'jid-1', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-1'])
+        self.assertEqual(res['pending'], {})
+
+        self.lua('put', 0, None, 'queue', 'jid-2', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-1', 'jid-2'])
+        self.assertEqual(res['pending'], {})
+
+        self.lua('put', 0, None, 'queue', 'jid-3', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-1', 'jid-2'])
+        self.assertEqual(res['pending'], ['jid-3'])
+
+        self.lua('resource.set', 0, 'r-1', 3)
+        self.lua('pop', 2, 'queue', 'worker-1', 1)
+        self.lua('complete', 3, 'jid-2', 'worker-1', 'queue', {})
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-1', 'jid-3'])
+        self.assertEqual(res['pending'], {})
+
+    def test_handles_decreasing_resource_limit(self):
+        """Resource decrease will eventually lower as work processes"""
+        self.lua('resource.set', 0, 'r-1', 3)
+
+        self.lua('put', 0, None, 'queue', 'jid-1', 'klass', {}, 0, 'resources', ['r-1'])
+        self.lua('put', 0, None, 'queue', 'jid-2', 'klass', {}, 0, 'resources', ['r-1'])
+        self.lua('put', 0, None, 'queue', 'jid-3', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-1', 'jid-2', 'jid-3'])
+        self.assertEqual(res['pending'], {})
+        self.lua('resource.set', 0, 'r-1', 1)
+        jobs = self.lua('pop', 2, 'queue', 'worker-1', 3)
+
+        self.lua('complete', 3, 'jid-1', 'worker-1', 'queue', {})
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-2', 'jid-3'])
+        self.assertEqual(res['pending'], {})
+
+        self.lua('complete', 3, 'jid-2', 'worker-1', 'queue', {})
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-3'])
+        self.assertEqual(res['pending'], {})
+
+        self.lua('complete', 3, 'jid-3', 'worker-1', 'queue', {})
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], {})
+        self.assertEqual(res['pending'], {})
+
+        self.lua('put', 0, None, 'queue', 'jid-4', 'klass', {}, 0, 'resources', ['r-1'])
+        self.lua('put', 0, None, 'queue', 'jid-5', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-4'])
+        self.assertEqual(res['pending'], ['jid-5'])
+
+    def test_handles_setting_resource_limit_to_zero(self):
+        """Resource decrease will eventually lower as work processes"""
+        self.lua('resource.set', 0, 'r-1', 1)
+
+        self.lua('put', 0, None, 'queue', 'jid-1', 'klass', {}, 0, 'resources', ['r-1'])
+        self.lua('put', 0, None, 'queue', 'jid-2', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], ['jid-1'])
+        self.assertEqual(res['pending'], ['jid-2'])
+
+        self.lua('resource.set', 0, 'r-1', 0)
+        self.lua('pop', 2, 'queue', 'worker-1', 1)
+        self.lua('complete', 3, 'jid-1', 'worker-1', 'queue', {})
+
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], {})
+        self.assertEqual(res['pending'], ['jid-2'])
+
+        jobs = self.lua('pop', 2, 'queue', 'worker-1', 1)
+        self.assertEqual(jobs, {})
+
+        self.lua('put', 0, None, 'queue', 'jid-3', 'klass', {}, 0, 'resources', ['r-1'])
+        res = self.lua('resource.get', 0, 'r-1')
+        self.assertEqual(res['locks'], {})
+        self.assertEqual(res['pending'], ['jid-3', 'jid-2'])
